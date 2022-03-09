26717b821608cf6df1d7c2d93a10701d2a9383c2
DRodrigues17 <adanielluiz71@gmail.com>
2022-03-09 20:23:42 -0300

    projeto criado

---
+++ backend/src/test/java/com/desvsuperior/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desvsuperior.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
